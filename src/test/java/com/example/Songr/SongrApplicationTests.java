48977be5984091fb7d7d5ef9ba2df65614290481
suhibabbas <sohaib_salah96@yahoo.com>
2022-03-19 00:06:46 +0300

    initial commit from Spring init

---
+++ src/test/java/com/example/Songr/SongrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Songr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
