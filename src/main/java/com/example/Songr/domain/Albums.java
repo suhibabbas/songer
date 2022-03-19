006afa660c7bd6d7ecc30b7a977f083be5c179e1
suhibabbas <sohaib_salah96@yahoo.com>
2022-03-19 20:00:41 +0300

    make the album, hello and capitalize root

---
+++ src/main/java/com/example/Songr/domain/Albums.java
@@ -0,0 +1,48 @@
+package com.example.Songr.domain;
+
+public class Albums {
+    private String title;
+    private String artist;
+    private int songCount;
+    private int length;
+    private String imageUrl;
+
+    public Albums(String title, String artist, int songCount, int length, String imageUrl) {
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Albums{" +
+                "title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                ", songCount=" + songCount +
+                ", length=" + length +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
