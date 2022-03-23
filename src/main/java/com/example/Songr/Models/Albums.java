b4a50bbf12d53c5ade1e94615fd66a7c5a92644c
suhibabbas <sohaib_salah96@yahoo.com>
2022-03-23 20:37:34 +0300

    the add connected to the postgressql data base and can add a new album to it and render it

---
+++ src/main/java/com/example/Songr/Models/Albums.java
@@ -0,0 +1,79 @@
+package com.example.Songr.Models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+
+public class Albums {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String title;
+    private String artist;
+    private int songCount;
+    private int length;
+    private String imageUrl;
+
+    public Albums() {
+    }
+
+    public Albums(String title, String artist, int songCount, int length, String imageUrl) {
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public void setSongCount(int songCount) {
+        this.songCount = songCount;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
--- src/main/java/com/example/Songr/web/SongrController.java
+++ src/main/java/com/example/Songr/web/SongrController.java
@@ -1,23 +1,25 @@
 package com.example.Songr.web;
 
 
-import com.example.Songr.domain.Albums;
+import com.example.Songr.Models.Albums;
+import com.example.Songr.Repositries.SongrRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class albumController {
+public class SongrController {
 
+    @Autowired
+    SongrRepository songrRepository;
 
-//    @ResponseStatus(value = HttpStatus.OK)
+    @ResponseStatus(value = HttpStatus.OK)
     @GetMapping("/hello")
     String hello (@RequestParam(name = "name",required = false,defaultValue = "user")String name, Model model){
         model.addAttribute("name",name);
@@ -31,14 +33,24 @@ public class albumController {
         model.addAttribute("upperCase",upperCase);
         return "capitalize";
     }
+    @PostMapping("/addalbum")
+    public RedirectView addAlbum(@ModelAttribute Albums albums){
+        songrRepository.save(albums);
+        return new RedirectView("allAlbum");
+    }
+    
+
+    @GetMapping("/allAlbum")
+    public String gitAllAlbum(Model model){
+        model.addAttribute("albumsList",songrRepository.findAll());
+        return "index";
+    }
 
     @ResponseBody
     @GetMapping("/Album")
     List<Albums> albums(Model model){
         List<Albums>albums= new ArrayList<>();
-        albums.add(new Albums("Godzilla","Eminem",20,240,"Link"));
-        albums.add(new Albums("Bad","Michael Jackson",11,320,"Link"));
-        albums.add(new Albums("This is Acting","Sia",9,260,"Link"));
+//        albums.add(new Albums("Godzila","Eminem",20,240,"Link"));
         return albums;
     }
 
